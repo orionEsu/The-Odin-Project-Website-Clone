019a83fad9a96f6dfcfeb45412aac9aaf353e816
Esu Gabriel <orionesu2001@gmail.com>
2020-08-21 14:43:38 +0100

    Added Functionality to Navigation Menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 const navSlide = () =>{
+    const burger = document.querySelector('.burger')
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav__list');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-close');
+    });
 
     hamburger.addEventListener('click', () => {
         // toggle nav
         nav.classList.toggle('nav-active');
     });
+
 }
 
 
