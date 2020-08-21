eec3b6f64f33270d8b5eac7d121e8953b3492a81
Esu Gabriel <orionesu2001@gmail.com>
2020-08-21 21:41:56 +0100

    Fixed Bugs

--- js/script.js
+++ js/script.js
@@ -2,16 +2,21 @@ const navSlide = () => {
     const burger = document.querySelector('.burger')
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav__list');
+      
 
-
-    hamburger.addEventListener('click', () => {
+    hamburger.addEventListener('click', (e) => {
         // toggle nav
+        
         nav.classList.toggle('nav-active');
-    });
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-close');
+       
     });
 
+    burger.addEventListener('click', (e) => {
+        e.preventDefault();
+        nav.classList.remove('nav-active');
+    });
+    
+    
 }
 
 
