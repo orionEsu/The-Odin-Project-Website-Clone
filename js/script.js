4f1ef85b525883a702729f156d1d90e848a58ce4
Esu Gabriel <orionesu2001@gmail.com>
2020-08-27 23:13:35 +0100

    Added Features

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const navSlide = () => {
     const burger = document.querySelector('.burger')
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav__list');
+    const check = document.querySelector('.checkbox');
+    const tick = document.querySelector('.tick');
    
     hamburger.addEventListener('click', (e) => {
         // toggle nav
@@ -14,6 +16,18 @@ const navSlide = () => {
         e.preventDefault();
         nav.classList.remove('nav-active');
     });
+
+    
+
+    check.addEventListener('click', () => {
+
+        tick.classList.toggle('active');
+        
+        
+    })
+
+
+    
     
 }
 
